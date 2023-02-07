97d869cef00707a45412a746f26d7f0841212972
Laurel McLean <laurel.mclean@live.ca>
2023-02-06 20:47:59 -0700

    Added Users and Forms

---
+++ grocery_app/__init__.py
@@ -0,0 +1,25 @@
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+from grocery_app.models import User
+from grocery_app.extensions import app
+
+
+###########################
+# Authentication
+###########################
+
+# authentication setup code
+
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
+
+# This tells the login manager how to load a user with a particular id
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+
+# initialize Bcrypt for hashing passwords:
+bcrypt = Bcrypt(app)
--- grocery_app/extensions.py
+++ grocery_app/extensions.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 from grocery_app.config import Config
 import os
 
